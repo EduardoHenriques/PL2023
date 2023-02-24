db579fd3f6830aa5f742c7217acf4fa6d6345127
EduardoHenriques <61889790+EduardoHenriques@users.noreply.github.com>
2023-02-24 18:29:10 +0000

    TPC2 feito

--- TPC2/TPC2.py
+++ TPC2/TPC2.py
@@ -1,1 +1,31 @@
+import sys
+import re
+
+if __name__ == '__main__':
+        switch = True
+        total = 0
+        #procura on/ON
+        look_On = re.compile(r'(?i)On')
+        #procura off/OFF
+        look_Off = re.compile(r'(?i)Off')
+        
+        for line in sys.stdin:
+            line = line.strip()
+            if re.search(look_On, line):
+                switch = True
+                print("Sum on")
+            elif re.search(look_Off, line):
+                switch = False
+                print("Sum off")
+
+            if switch:
+                nums = re.findall("\d+", line)
+                for n in nums:
+                    total += int(n)
+
+            if "=" in line:
+                print(f"Resultado: {total}")
+                sum_digits = 0
+
+
 
