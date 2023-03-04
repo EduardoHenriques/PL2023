6a94e682c36ada7dd84324d046876b19ad7b57a6
EduardoHenriques <fernando28henriques@gmail.com>
2023-03-04 21:23:19 +0000

    a) b) c) feito

--- TPC3/TPC3.py
+++ TPC3/TPC3.py
@@ -1,6 +1,12 @@
 import re
 import pprint
-#abc… 	Letters
+import math
+import sys
+import statistics
+from statistics import mode
+from statistics import multimode
+import collections
+# abc… 	Letters
 #	123… 	Digits
 #	\d 	Any Digit
 #	\D 	Any Non-digit character
@@ -24,23 +30,119 @@ import pprint
 #	(a(bc)) 	Capture Sub-group
 #	(.*) 	Capture all
 #	(abc|def) 	Matches abc or def
-#Dentro dos [] os caracteres especiais perdem o efeito
+# Dentro dos [] os caracteres especiais perdem o efeito
 
 def datas(txt):
 	regexDatas = re.compile(r'([0-9]+)-[0-9]+-[0-9]')  # ::ANO-MES-DIA::
 	res = dict()
-	matches = re.findall(regexDatas,txt)
-	for match in matches:
-		if match in res.keys():
-			res[match] += 1
+	for line in txt:
+		matches = re.findall(regexDatas,line)
+		for match in matches:
+			if match in res.keys():
+				res[match] += 1
+			else:
+				res[match] = 1
+		return res
+
+
+def list_to_dict(list):
+	res = dict()
+	for elem in list:
+		if elem in res.keys():
+			res[elem] += 1
 		else:
-			res[match] = 1
+			res[elem] = 1
 	return res
+
+
+def nomes(txt):
+	resProprios = dict()
+	resApelidos = dict()
+
+	regProprio = re.compile(r'::([a-zA-Z]+) ')
+	regApelido = re.compile(r'([a-zA-Z]+)::')
+	regAno = re.compile(r'::(\d+)-\d+-\d+')
+
+	apelidosTotais = []
+	nomesPropriosTotais = []
+
+	for line in txt:
+		nomesProprios = re.findall(regProprio,line)
+		Apelidos = re.findall(regApelido, line)
+		Ano = re.findall(regAno, line)
+
+		if  len(Ano) > 0:
+			seculo = math.floor(int(Ano[0])/100) + 1
+			seculo = "Século " + str(seculo)
+			if seculo not in resProprios.keys():
+				resProprios[seculo] = []
+			else:
+				resProprios[seculo] += nomesProprios
+			nomesPropriosTotais += nomesProprios
+			if seculo not in resApelidos.keys():
+				resApelidos[seculo] = []
+			else:
+				resApelidos[seculo] += Apelidos
+			apelidosTotais += Apelidos
+
+
+	counterNP = collections.Counter(nomesPropriosTotais)
+	counterA = collections.Counter(apelidosTotais)
+	most_common_NP = counterNP.most_common(5)
+	most_common_A = counterA.most_common(5)
+
+	for key in resProprios:
+		resProprios[key] = list_to_dict(resProprios[key])
+		#print(resProprios)
+	for key in resApelidos:
+		resApelidos[key] = list_to_dict(resApelidos[key])
+	return (resProprios,resApelidos,most_common_NP,most_common_A)
+
+
+def familiares(text):
+	dict_idades = dict()
+	blacklist = ['fls','doc']
+	regFamiliares = re.compile(r',([A-Z][a-zA-Z ]+)\.')
+	for line in text:
+		familiares = re.findall(regFamiliares,line)
+		for elem in familiares:
+			if elem not in blacklist:
+				if elem in dict_idades.keys():
+					dict_idades[elem] += 1
+				else:
+					dict_idades[elem] = 1
+
+
+	return dict_idades
+
+
+def toJSON(text):
+
 
 
 if __name__ == "__main__":
 	pp = pprint.PrettyPrinter(indent=4)
 	with open("processos.txt") as file:
-		text = file.read()
-	dict_datas = datas(text)
-	pp.pprint(dict_datas)
+		text = file.readlines()
+
+		for opcao in sys.stdin:
+			opcao = int(opcao)
+			if opcao == 1:
+				pp.pprint(datas(text))
+			if opcao == 2:
+				resNomes = nomes(text)
+				for key in resNomes[0]:
+					print(f"NOMES PROPRIOS {key}")
+					pp.pprint(resNomes[0][key])
+
+				for key in resNomes[1]:
+					print(f"APELIDOS {key}")
+					pp.pprint(resNomes[1][key])
+
+				print(f"NOMES PROPRIOS MAIS COMUNS:{resNomes[2]}")
+				print(F"APELIDOS MAIS COMUNS:{resNomes[3]}")
+			if opcao == 3:
+				pp.pprint((familiares(text)))
+			if opcao == 4:
+				toJSON(text[0:20])
+
