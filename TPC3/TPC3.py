3fbc7583354dec85ae8f3be2246566dbd99959c9
EduardoHenriques <fernando28henriques@gmail.com>
2023-03-10 13:39:30 +0000

    Parte basica do TPC4

--- TPC3/TPC3.py
+++ TPC3/TPC3.py
@@ -3,35 +3,8 @@ import re
 import pprint
 import math
 import sys
-import statistics
-from statistics import mode
-from statistics import multimode
 import collections
-# abc… 	Letters
-#	123… 	Digits
-#	\d 	Any Digit
-#	\D 	Any Non-digit character
-#	. 	Any Character
-#	\. 	Period
-#	[abc] 	Only a, b, or c
-#	[^abc] 	Not a, b, nor c
-#	[a-z] 	Characters a to z
-#	[0-9] 	Numbers 0 to 9
-#	\w 	Any Alphanumeric character
-#	\W 	Any Non-alphanumeric character
-#	{m} 	m Repetitions
-#	{m,n} 	m to n Repetitions
-#	* 	Zero or more repetitions
-#	+ 	One or more repetitions
-#	? 	Optional character
-#	\s 	Any Whitespace
-#	\S 	Any Non-whitespace character
-#	^…$ 	Starts and ends
-#	(…) 	Capture Group
-#	(a(bc)) 	Capture Sub-group
-#	(.*) 	Capture all
-#	(abc|def) 	Matches abc or def
-# Dentro dos [] os caracteres especiais perdem o efeito
+
 
 def datas(txt):
 	regexDatas = re.compile(r'([0-9]+)-[0-9]+-[0-9]')  # ::ANO-MES-DIA::
@@ -161,9 +134,6 @@ if __name__ == "__main__":
 					print(f"NOMES PROPRIOS {key}")
 					print(f"{resNomes[0][key]}||")
 
-				#for key in resNomes[1]:
-				#	print(f"APELIDOS {key}")
-				#	pp.pprint(resNomes[1][key])
 
 				print(f"NOMES PROPRIOS MAIS COMUNS:{resNomes[2]}")
 				print(F"APELIDOS MAIS COMUNS:{resNomes[3]}")
--- TPC4/TPC4.py
+++ TPC4/TPC4.py
@@ -1,1 +1,38 @@
+#  (?<=er1)er quando tiver er1 antes
+#  (?<!er1) er quando nao tiver er1 antes
+# exemplo: palavras acabadas em mente ->  (?<=[a-z]+)mente
+import sys
+import re
+import json
+import unidecode
 
+def toJson(text):
+	listDicionarios = []
+	#  comecar por obter uma "template" do dicionario que vai ser usado em cada linha
+	splitParams = re.compile(r'([^,]+)[,\n]')
+	paramList = re.findall(splitParams, text[0])
+	
+	
+	#encher uma copia do dicionario por linha
+	
+	for i in range(1, len(text)):
+		matches = re.findall(splitParams, text[i])
+		matches = [unidecode.unidecode(m) for m in matches]
+
+		if len(matches) == len(paramList):
+			dictLine = dict(zip(paramList, matches))
+			print(json.dumps(dictLine))
+
+
+if __name__ == "__main__":
+	dir = "teste.csv"
+	try:
+		with open(dir) as file:
+			lines = file.readlines()
+		if len(lines) == 0:
+			print("Erro, ficheiro vazio")
+		else:
+			toJson(lines)
+		
+	except OSError:
+		print(f"Ficheiro Invalido -> {dir}")
